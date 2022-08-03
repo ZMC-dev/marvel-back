ca7dddd10fa506d37f24199132732eec0bb9b4ab
ZMC-dev <zmc.dev2019@gmail.com>
2022-08-03 18:29:29 +0200

    add .env + .gitignore

--- server.js
+++ server.js
@@ -1,16 +1,31 @@
 const express = require("express");
 const cors = require("cors");
 
+const axios = require("axios");
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+require('dotenv').config();
 
-app.get("/comics", (req, res) => {
-  console.log("route /comics");
-  res.status(200).json({ message: " la route /comics marche ;)" });
+//const apiKey = "0tIXVnEh3FhxCmBv"
 
-});
+
+app.get("/comics", async (req, res) => {
+  try {
+    const response = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=${process.env.API_KEY}`
+    );
+    console.log("route /comics");
+    res.status(200).json(response.data);
+  
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+    }
+  });
+
+
 
 app.get("/comics/:characterId", (req, res) => {
   console.log("route /comics/:characterId");
